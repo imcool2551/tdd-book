1baf7560240e3b492b6995f0cb5dc371e02dbec7
SangWook <imcool2551@gmail.com>
2022-01-18 14:17:55 +0900

    chap03: ExpiryDateCalculator Refactoring

--- chap01-idea/src/test/java/chap03/ExpiryDateCalculator.java
+++ chap01-idea/src/test/java/chap03/ExpiryDateCalculator.java
@@ -4,7 +4,7 @@ import java.time.LocalDate;
 
 public class ExpiryDateCalculator {
 
-    public LocalDate calculateExpiryDate(LocalDate billingDate, int payAmount) {
-        return LocalDate.of(2019, 4, 1);
+    public LocalDate calculateExpiryDate(PayData payData) {
+        return payData.getBillingDate().plusMonths(1);
     }
 }
